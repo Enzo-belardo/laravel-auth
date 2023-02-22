e070d31906990a7a1b00ff7101243fefc2cb7284
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-02-22 18:47:59 +0100

    add: thumb in card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,13 @@
 
 
 <div class="container">
-    <div class="text-center mb-3 text-danger">
+    <div class="text-center mb-3 text-primary p-3">
         <h1>I miei progetti</h1>
     </div>
     <div class="d-flex justify-content-between flex-wrap">
         @foreach($projects as $project)
         <div class="card mb-3" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="{{$project->thumb}}" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">{{$project->title}}</h5>
               <p class="card-text">{{$project->programming_language}}</p>
